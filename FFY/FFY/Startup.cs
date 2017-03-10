8c13f113c0e402afc0860973758933d64ce733a6
Tsvetoslav Lazarov <nearfs@abv.bg>
2017-03-10 10:39:04 +0200

    Initial

---
+++ FFY/FFY/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FFY.Web.Startup))]
+namespace FFY.Web
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
