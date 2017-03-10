d826de70c997dfbedf1e8d22e9f8151ae8de6092
Tsvetoslav Lazarov <nearfs@abv.bg>
2017-03-10 11:26:22 +0200

    Extracted Identity Config in new project and added Ninject MVC.

--- FFY/FFY.Models/Address.cs
+++ FFY/FFY.Models/Address.cs
@@ -1,12 +1,46 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 namespace FFY.Models
 {
-    class Address
+    public class Address
     {
+        private ICollection<Order> orders;
+        public Address()
+        {
+            this.Orders = new HashSet<Order>();
+        }
+
+        public Address(string street, string city, string country) : this()
+        {
+            this.Street = street;
+            this.City = city;
+            this.Country = country;
+        }
+
+        [Key]
+        public int Id { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        [MaxLength(20)]
+        public string Street { get; set; }
+
+        public string City { get; set; }
+
+        public string Country { get; set; }
+
+        public virtual ICollection<Order> Orders
+        {
+            get
+            {
+                return this.orders;
+            }
+
+            set
+            {
+                this.orders = value;
+            }
+        }
     }
 }
