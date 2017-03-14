7f2faacecd91d9dd7dc6094b72e16c268ae3c50a
Tsvetoslav Lazarov <nearfs@abv.bg>
2017-03-14 16:09:02 +0200

    Added unit tests folder and trying Jenkins jobs.

---
+++ FFY/FFY.UnitTests/Test.cs
@@ -0,0 +1,19 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FFY.UnitTests
+{
+    [TestFixture]
+    public class Test
+    {
+        [Test]
+        public void RandomTest()
+        {
+            Assert.IsTrue(true);
+        }
+    }
+}
