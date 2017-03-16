e7f00dfc40252a91d34a6bc441cb83b77c8d9840
Tsvetoslav Lazarov <nearfs@abv.bg>
2017-03-16 18:00:15 +0200

    Added Administration area and base base management pages.

---
+++ FFY/FFY/Areas/Administration/Controllers/ContactManagementController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FFY.Web.Areas.Administration.Controllers
+{
+    public class ContactManagementController : Controller
+    {
+        // GET: Administration/ContactManagement
+        public ViewResult Index()
+        {
+            return View();
+        }
+    }
+}
