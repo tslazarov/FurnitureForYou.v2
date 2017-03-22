a3cb4f10cf4ea29f2cab3037661342a8bbe9257e
Tsvetoslav Lazarov <nearfs@abv.bg>
2017-03-22 14:32:43 +0200

    Support chat 3.5/5 done.

--- FFY/FFY/SupportChat/SupportChatHub.cs
+++ FFY/FFY/SupportChat/SupportChatHub.cs
@@ -4,9 +4,11 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Threading.Tasks;
+using Microsoft.AspNet.SignalR.Hubs;
 
 namespace FFY.Web.SupportChat
 {
+    [HubName("supportChatHub")]
     public class SupportChatHub : Hub
     {
         public void SendMessage(string sender, string receiver, string message)
@@ -19,7 +21,7 @@ namespace FFY.Web.SupportChat
             {
                 this.Clients.Group(sender).addMessage(sender, message);
             }
-            this.Clients.Group("support").addMessage(sender, message);
+            this.Clients.Group("support").addMessage(sender, message, receiver);
         }
 
         public override Task OnConnected()
@@ -28,14 +30,35 @@ namespace FFY.Web.SupportChat
 
             if (this.Context.User.IsInRole("Administrator"))
             {
+                // repository add
                 Groups.Add(this.Context.ConnectionId ,"support");
+                this.Clients.Group("support").connectSupport(name);
             }
             else
             {
+                // repository add
                 Groups.Add(this.Context.ConnectionId, name);
+                this.Clients.Group("support").connectClient(name);
             }
 
             return base.OnConnected();
+        }
+
+        public override Task OnDisconnected(bool stopCalled)
+        {
+            string name = this.Context.User.Identity.Name;
+            if (this.Context.User.IsInRole("Administrator"))
+            {
+                // repository remove
+                this.Clients.Group("support").disconnectSupport(name);
+            }
+            else
+            {
+                // repository remove
+                this.Clients.Group("support").disconnectClient(name);
+            }
+
+            return base.OnDisconnected(stopCalled);
         }
     }
 }
